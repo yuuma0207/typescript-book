5e4fb76a943a433c3eb6d1ea8fc29c0a2d1d6fd3
yuuma <yuuma0207@gmail.com>
2025-01-02 01:13:25 +0900

    add: 4.1.3

--- src/chap4/4-1.ts
+++ src/chap4/4-1.ts
@@ -25,3 +25,22 @@ function helloWorldNTimes(n: number): void {
 
 helloWorldNTimes(5)
 helloWorldNTimes(121)
+
+// 4.1.3
+type Human = {
+    height: number;
+    weight: number;
+}
+
+// const calcBMI = function(human: Human): number {
+//     return human.weight/human.height**2;
+// };
+
+const calcBMI = function({ height, weight }: Human): number {
+    return weight/height**2;
+};
+
+const uhyo: Human = { height: 1.84, weight: 72 }
+
+console.log(calcBMI(uhyo))
+
